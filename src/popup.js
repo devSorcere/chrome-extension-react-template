ec6f439727d785a271589fa257d28bde1b62003d
Waris Ahmed <warisahmed2022@gmail.com>
2023-11-18 04:03:46 +0500

    first commit

---
+++ src/popup.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { render } from "react-dom";
+
+
+function Popup() {
+    return (
+        <>
+            <div>
+                <h1>Dummy</h1>
+            </div>
+        </>
+    )
+}
+
+
+render(<Popup />, document.getElementById('react-target'))
